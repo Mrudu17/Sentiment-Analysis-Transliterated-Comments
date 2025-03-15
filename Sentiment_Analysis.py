ed4164905c6258b142f552c3848870a08bad4bfc
S.K.Mruduvani <s.mruduvani@gmail.com>
2025-03-15 21:30:48 +0530

    Update Sentiment_Analysis.py

--- Sentiment_Analysis.py
+++ Sentiment_Analysis.py
@@ -163,13 +163,13 @@ def social_button(icon_path, label, key):
 
 
 with col1:
-    social_button("images\Youtube.jpeg", "Youtube", "youtube")
+    social_button("images/Youtube.jpeg", "Youtube", "youtube")
 with col2:
-    social_button("images\X.jpeg", "⠀⠀X⠀⠀", "twitter")
+    social_button("images/X.jpeg", "⠀⠀X⠀⠀", "twitter")
 with col3:
-    social_button("images\Instagram.jpeg", "Instagram", "ig")
+    social_button("images/Instagram.jpeg", "Instagram", "ig")
 with col4:
-    social_button("images\Facebook.jpeg", "Facebook", "fb")
+    social_button("images/Facebook.jpeg", "Facebook", "fb")
 
 if "platform_selected" not in st.session_state:
     st.session_state.platform_selected = None
